3c112feccb59eceb33e11e567d22d4e95c25a5cd
Nasir <nasirkhimani@gmail.com>
2023-03-27 13:55:34 -0400

    moved source to a seperate class

---
+++ BlackJackPrototype/Game.cpp
@@ -0,0 +1,235 @@
+#include "Game.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+Game::Game()
+{
+	bal = 0;
+	betammount = 0;
+
+	fstream scoresfile("blackjackscores.txt");
+	if (scoresfile.is_open())
+	{
+		scoresfile >> bal;
+		scoresfile.close();
+	}
+	else {
+		scoresfile.close();
+		bal = 100;
+		ofstream _("blackjackscores.txt");
+		_ << bal;
+		_.close();
+	}
+	cout << bal;
+}
+
+Game::~Game()
+{
+	ofstream scoresfilev2("blackjackscores.txt", ios::trunc);
+	scoresfilev2 << bal;
+	scoresfilev2.close();
+}
+void Game::show(Hand* ph, Hand* dh)
+{
+	system("cls");
+	cout << "Dealers Cards" << endl;
+	cout << *dh << endl;
+	cout << "Player Hands" << endl;
+	cout << *ph << endl;
+}
+
+
+
+char Game::ask()
+{
+	cout << "Hit or stand: ";
+	char choice;
+	cin >> choice;
+	return choice;
+}
+
+
+Game::move_returns Game::move(bool stand, Hand* ph, Hand* dh, Deck* d) // PLAYERBUST if you cant move after last move...
+{
+	if (stand)
+	{
+		//if standing do dealer logic
+		dh->getHandptr()[0]->setFaceup(false);
+		d->Draw(dh);
+		show(ph, dh);
+		if (dh->getHand_Value() > 16)
+		{
+			return DELEAR16;
+		}
+		else if (dh->getHand_Value() > 21)
+		{
+			return DEALERBUST;
+		}
+		else
+		{
+			return OK;
+		}
+	}
+	else
+	{
+		//hit logic for player
+		d->Draw(ph);
+		show(ph, dh);
+		return ph->getHand_Value() > 21 ? PLAYERBUST : OK;
+	}
+
+}
+
+
+void Game::resultofgame() {
+
+
+	double multiplier = 0;
+	if (gamestate > 3)
+	{
+		multiplier = 1.5;
+	}
+	else
+	{
+		multiplier = -1;
+
+	}
+	bal = bal + (multiplier * betammount);
+
+	switch (int(gamestate))
+	{
+	case 0:
+
+		cout << "Instant loss by dealer\n";
+		cout << "Your new balance is " << bal << endl;
+
+		break;
+	case 1:
+		cout << "Loss by dealer\n";
+		cout << "Your new balance is " << bal << endl;
+
+		break;
+	case 2:
+		cout << "You have gone over 21, you loss\n";
+		cout << "Your new balance is " << bal << endl;
+
+		break;
+	case 3:
+		cout << "The dealer has busted you win!\n";
+		cout << "Your new balance is " << bal << endl;
+
+		break;
+	case 4:
+		cout << "Your cards are closest to 21!, you win\n";
+		cout << "Your new balance is " << bal << endl;
+
+		break;
+	case 5:
+		cout << "You got blackjack!, you win\n";
+		cout << "Your new balance is " << bal << endl;
+		break;
+	case 6:
+		cout << "insufficient funds.";
+		break;
+	default:
+		break;
+	}
+
+}
+
+void Game::PlayGame()
+{
+	Deck* deck = new Deck();
+	Hand* phand = new Hand();
+	Hand* dhand = new Hand();
+
+
+	cout << "Welome to the game" << endl;
+	cout << "Enter ammount to bet: ";
+	cin >> betammount;
+	if (bal - betammount < 0)
+	{
+		gamestate = NOT_ENOUGH_MONEY;
+	}
+	cout << "The dealer has:" << endl;
+	deck->Draw(dhand);
+	deck->Draw(dhand);
+	dhand->getHandptr()[0]->setFaceup(true);
+	cout << *dhand;
+	cout << dhand->getHand_Value() << endl;
+	cout << "You have: " << endl;
+	deck->Draw(phand);
+	deck->Draw(phand);
+	cout << *phand;
+	cout << phand->getHand_Value() << endl;
+	//preflip bj check
+	if (phand->getHand_Value() == 21)
+	{
+		dhand->getHandptr()[0]->setFaceup(false);
+		show(phand, dhand);
+		gamestate =  INSTANT_WIN;
+	}
+	else if (dhand->getHand_Value() == 21)
+	{
+		dhand->getHandptr()[0]->setFaceup(false);
+		show(phand, dhand);
+		gamestate = INSTANT_LOSS;
+	}
+
+	bool stand = false;
+	move_returns cont = OK;
+	char choice = 's';
+	while (cont == OK)
+	{
+		if (!stand)
+		{
+			cout << "Hit or stand: ";
+			cin >> choice;
+		}
+		switch (choice)
+		{
+			case int('h') :
+				cont = move(false, phand, dhand, deck);
+				break;
+				case int('s') :
+					cont = move(true, phand, dhand, deck);
+					stand = true;
+					break;
+				default:
+					cout << "Invalid command" << endl;
+					break;
+		}
+	}
+	//after this line no more cards are added
+
+	if (cont == DEALERBUST)
+	{
+		gamestate = WINBYBUST;
+	}
+	else if (cont == PLAYERBUST)
+	{
+		gamestate = LOSSBYBUST;
+	}
+	else // this is if dealer16
+	{
+
+		if (dhand->getHand_Value() > 21)
+		{
+			gamestate = WINBYSELF;
+		}
+		else if (dhand->getHand_Value() > phand->getHand_Value())
+		{
+			gamestate = LOSSBYDEALER;
+		}
+		else
+		{
+			gamestate = WINBYSELF;
+		}
+	}
+	resultofgame();
+};
+
